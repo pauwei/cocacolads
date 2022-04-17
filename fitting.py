0bedc27d6d8367149d01bc7accf6fad26dc6f436
jenny <jenny.xie@ufl.edu>
2022-04-17 00:30:37 -0400

    updated fitting.py

--- fitting.py
+++ fitting.py
@@ -1,3 +1,14 @@
 # Based on calculated statistics find whether classification or regression is better
 
 # Create model or regression line for prediction
+import testing as testing
+import matplotlib.pyplot as plt
+import numpy as np 
+
+def regression_plot():
+    x,y = testing.get_combined_list()
+    plt.scatter(x, y)
+    b,a = np.polyfit(x,y, deg=1)
+    xseq = np.linspace(0,10, num=100)
+    plt.plot(xseq, a + b * xseq, color ='k')
+    plt.show()
