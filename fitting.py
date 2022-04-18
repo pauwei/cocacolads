c5f323ce9ef0c620e732abc86e44a6dcef0b73db
jenny <jenny.xie@ufl.edu>
2022-04-17 20:45:33 -0400

    updated regression in fitting.py

--- fitting.py
+++ fitting.py
@@ -5,10 +5,10 @@ import testing as testing
 import matplotlib.pyplot as plt
 import numpy as np 
 
-def regression_plot():
+def regression_line():
     x,y = testing.get_combined_list()
     plt.scatter(x, y)
     b,a = np.polyfit(x,y, deg=1)
-    xseq = np.linspace(0,10, num=100)
+    xseq = np.linspace(0,100, num=100)
     plt.plot(xseq, a + b * xseq, color ='k')
     plt.show()
